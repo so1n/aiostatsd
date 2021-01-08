1c293e1076248dd4edf22c01c63b4b10bd1ba98b
So1n <qaz6803609@163.com>
2021-01-08 19:15:03 +0800

    Modify, refactor, statsd&dogstatsd protocol

--- aio_statsd/protocol.py
+++ aio_statsd/protocol.py
@@ -11,84 +11,63 @@ class StatsdProtocol(object):
     https://github.com/statsd/statsd/blob/master/docs/metric_types.md
     """
     def __init__(self, prefix: Optional[str] = None, mtu_limit: int = 1432):
-        if prefix:
-            self.msg = f'{prefix}.'
-        else:
-            self.msg = ''
+        self.msg = f'{prefix}.' if prefix else ''
         self._mtu_limit = mtu_limit
 
-    def _msg_handle(self, msg: str):
+    def _msg_handle(self, msg: str) -> 'StatsdProtocol':
         if self.msg == '':
             self.msg = msg
         else:
             self.msg += '\n' + msg
         if len(self.msg) > self._mtu_limit:
             logging.warning(
-                f'msg length > mtu max length limit:{self._mtu_limit}'
+                f'msg length:{len(self.msg)} > mtu max length limit:{self._mtu_limit}'
                 f'learn more:https://github.com/statsd/statsd/blob/master/docs/metric_types.md'
             )
+        return self
 
     def counter(self, key: str, value: int) -> 'StatsdProtocol':
-        self._msg_handle(f'{key}:{value}|c')
-        return self
+        return self._msg_handle(f'{key}:{value}|c')
 
     def timer(self, key: str, value: int) -> 'StatsdProtocol':
-        self._msg_handle(f'{key}:{value}|ms')
-        return self
+        return self._msg_handle(f'{key}:{value}|ms')
 
     def gauge(self, key: str, value: int) -> 'StatsdProtocol':
-        self._msg_handle(f'{key}:{value}|g')
-        return self
+        return self._msg_handle(f'{key}:{value}|g')
 
     def sets(self, key: str, value: int) -> 'StatsdProtocol':
-        self._msg_handle(f'{key}:{value}|s')
-        return self
+        return self._msg_handle(f'{key}:{value}|s')
 
 
 class DogStatsdProtocol(object):
 
     def __init__(self, prefix: Optional[str] = None):
-        if prefix:
-            self._msg = f'{prefix}.'
-        else:
-            self._msg = ''
+        self._msg: str = f'{prefix}.' if prefix else ''
         self._cache: List[str] = []
 
     def get_msg_list(self) -> List[str]:
         return self._cache
 
-    @staticmethod
-    def build_tag(tag_dict: Optional[dict] = None):
-        if not tag_dict:
-            return ""
-        return "|#" + ",".join(f"{k}:{v}" for k, v in tag_dict.items())
+    def build_msg(self, key: str, value: int, type_: str, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
+        tag_str: str = "|#" + ",".join(f"{k}:{v}" for k, v in tag_dict.items()) if tag_dict else ""
+        msg: str = self._msg + f'{key}:{value}|{type_}' + tag_str
+        self._cache.append(msg)
+        return self
 
     def gauge(self, key: str, value: int, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
-        msg = self._msg + f'{key}:{value}|g' + self.build_tag(tag_dict)
-        self._cache.append(msg)
-        return self
+        return self.build_msg(key, value, 'g', tag_dict)
 
     def increment(self, key: str, value: int, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
-        msg = self._msg + f'{key}:{value}|c' + self.build_tag(tag_dict)
-        self._cache.append(msg)
-        return self
+        return self.build_msg(key, value, 'c', tag_dict)
 
     def decrement(self, key: str, value: int, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
-        msg = self._msg + f'{key}:-{value}|c' + self.build_tag(tag_dict)
-        self._cache.append(msg)
-        return self
+        return self.build_msg(key, -value, 'c', tag_dict)
 
     def timer(self, key: str, value: int, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
-        msg = self._msg + f'{key}:{value}|ms' + self.build_tag(tag_dict)
-        self._cache.append(msg)
-        return self
+        return self.build_msg(key, -value, 'ms', tag_dict)
 
     def histogram(self, key: str, value: int, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
-        msg = self._msg + f'{key}:{value}|h' + self.build_tag(tag_dict)
-        self._cache.append(msg)
-        return self
+        return self.build_msg(key, -value, 'h', tag_dict)
 
     def distribution(self, key: str, value: int, tag_dict: Optional[dict] = None) -> 'DogStatsdProtocol':
-        msg = self._msg + f'{key}:{value}|d' + self.build_tag(tag_dict)
-        self._cache.append(msg)
-        return self
+        return self.build_msg(key, -value, 'd', tag_dict)
